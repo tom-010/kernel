315a72edd00a85a0c877c2f53327b05bbc178aaf
Thomas Deniffel <tdeniffel@googlemail.com>
2018-02-01 17:08:16 +0100

    Simple echo works

---
+++ messages.c
@@ -0,0 +1,9 @@
+#include "io.c"
+
+void welcome_message(void) {
+    println("=====================================================");
+    println("|            Herzlich willkommen bei der            |");
+    println("|               BAVARIAN CODING GROUP               |");
+    println("=====================================================");
+    println("");
+}
